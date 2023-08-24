dfad021ed7aaf5695d663753c4c9956cb9866c58
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2023-08-24 16:31:51 +0200

    new fixes

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,12 +1,12 @@
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
-function Skeleton({
+function Skeleton ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn('animate-pulse rounded-md bg-muted', className)}
       {...props}
     />
   )
