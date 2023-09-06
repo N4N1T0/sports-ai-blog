c3723e29eda3a3d7ba8a06ed79c89e512cfbc9d0
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2023-09-06 11:42:13 +0200

    next/auth route complete

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,42 +1,40 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
-const handler = NextAuth({
+interface Credentials {
+  password: string
+}
+
+const handle = NextAuth({
   providers: [
     CredentialsProvider({
-    // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
+      // The name to display on the sign-in form (e.g., "Email")
+      name: 'Password',
       credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
         password: { label: 'Password', type: 'password' }
       },
-      async authorize (credentials, req) {
-      // You need to provide your own logic here that takes the credentials
-      // submitted and returns either a object representing a user or value
-      // that is false/null if the credentials are invalid.
-      // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-      // You can also use the `req` object to obtain additional parameters
-      // (i.e., the request IP address)
-        const res = await fetch('/your/endpoint', {
-          method: 'POST',
-          body: JSON.stringify(credentials),
-          headers: { 'Content-Type': 'application/json' }
-        })
-        const user = await res.json()
-
-        // If no error and we have user data, return it
-        if (res.ok && user) {
-          return user
+      authorize: async (credentials: Credentials) => {
+        if (credentials.password === 'Nan0123') {
+          const user = { id: 1, name: 'John' }
+          return await Promise.resolve(user)
         }
-        // Return null if user data could not be retrieved
-        return null
+        return await Promise.resolve(null)
       }
     })
-  ]
+  ],
+  // Redirect the user to the home page upon successful login
+  callbacks: {
+    async signIn (user, account, profile) {
+      return '/'
+    }
+  },
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60
+  },
+  database: {
+    // Configure database options here
+  }
 })
 
-export { handler as GET, handler as POST }
+export { handle as POST, handle as GET }
