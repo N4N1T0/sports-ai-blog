5f23a1f883640f8b67882b87a7374f0011fca11b
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2023-09-08 13:06:26 +0200

    tsconfig changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,15 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
-export const nextAuthOptions = {
+const handle = NextAuth({
   providers: [
     CredentialsProvider({
       name: 'Password',
       credentials: {
         password: { label: 'Password', type: 'password' }
       },
-      async authorize (credentials) {
-        if (credentials.password === 'Nan0123') {
+      async authorize (credentials, req) {
+        if (credentials?.password === 'Nan0123') {
           const user = { id: 1, name: 'John' }
           return user
         }
@@ -23,8 +23,6 @@ export const nextAuthOptions = {
     strategy: 'jwt',
     maxAge: 30 * 24 * 60 * 60
   }
-}
-
-const handle = NextAuth(nextAuthOptions)
+})
 
 export { handle as POST, handle as GET }
