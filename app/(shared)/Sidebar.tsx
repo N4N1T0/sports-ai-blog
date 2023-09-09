e3867b143f2b954814899a22d40d031ce0cc3e9e
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2023-09-09 11:08:19 +0200

    subscriber minor changes

--- app/(shared)/Sidebar.tsx
+++ app/(shared)/Sidebar.tsx
@@ -34,51 +34,35 @@ const Sidebar = ({ name, image, bio }: Author) => {
       </h4>
       {name === undefined
         ? (
-        <>
-          <ProfileCard
-            name='Adrian "Nano" Alvarez'
-            bio='Striking Coach with more than 10 years of experience'
-            image='https://res.cloudinary.com/dkvoatzeq/image/upload/v1694183461/ai-sports-blog/1_rthkte.jpg'
-          />
-          <hr className='border-1 border-black/10 dark:border-wh-10 mt-4'/>
-          <ProfileCard
-            name='Dayana "Good Day" Abuin'
-            bio='Taekwondo Black Belt with more than 5 years of sports journalism'
-            image='https://res.cloudinary.com/dkvoatzeq/image/upload/v1694183658/ai-sports-blog/WhatsApp_Image_2023-03-02_at_6.08.10_PM_ffoeyd.jpg'
-          />
-        </>
+          <div className='w-full flex flex-col justify-center items-center py-5 space-y-5'>
+            <h4 className='font-bold text-xl'>Striking Gold</h4>
+            <p className='leading-6'>Welcome to Striking Gold, your ultimate destination for all things combat sports, with a specialized focus on the thrilling world of Mixed Martial Arts (MMA). We are more than just a blog; we are a passionate community of fight enthusiasts, dedicated to bringing you the latest news, insightful analysis, and captivating stories from the front lines of combat sports.</p>
+            <hr className='border-1 w-full border-black/10 dark:border-wh-10'/>
+            <h4 className='font-bold text-xl'>Our Mission</h4>
+            <p className='leading-6'>At [Blog Name], our mission is clear: to provide you with the most comprehensive, engaging, and expertly-crafted content about MMA and combat sports. We are driven by our love for the sport and our commitment to delivering high-quality articles that cater to both the seasoned fight fan and the curious newcomer.</p>
+          </div>
           )
         : (
-        <ProfileCard
-          name={name}
-          bio={bio}
-          image={image}
-        />
+          <div className='flex flex-col justify-center mt-7'>
+              <Image
+                alt='about-profile'
+                src={image ?? '' }
+                width={500}
+                height={500}
+                style={{ width: '500px', height: '250px', objectFit: 'cover' }}
+                placeholder='blur'
+                blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mM8+R8AApcByuTu2nIAAAAASUVORK5CYII'
+              />
+                <h4 className='py-3 px-5 text-wh-500 dark:text-wh-100 font-bold text-center'>
+                {name}
+              </h4>
+              <p className='text-wh-500 text-center dark:text-wh-100 text-sm'>
+                {bio}
+              </p>
+          </div>
           )}
 
     </section>
-  )
-}
-
-const ProfileCard = ({ name, image, bio }: Author) => {
-  return (
-    <div className='flex flex-col justify-center mt-7'>
-        <Image
-          alt='about-profile'
-          src={image ?? ''}
-          width={500}
-          height={500}
-          style={{ width: '500px', height: '250px', objectFit: 'cover' }}
-          placeholder='blur'
-          blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mM8+R8AApcByuTu2nIAAAAASUVORK5CYII'
-        />
-          <h4 className='py-3 px-5 text-wh-500 dark:text-wh-100 font-bold text-center'>
-          {name}
-        </h4>
-        <p className='text-wh-500 text-center dark:text-wh-100 text-sm'>
-          {bio}
-        </p>
-    </div>
   )
 }
 
