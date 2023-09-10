66c425ef546804c19fe0537e93f0727da5d0ed1c
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2023-09-10 17:21:06 +0200

    remove shadcn and added new skeletons in post/id loading

--- app/about/loading.tsx
+++ app/about/loading.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
-
 const loading = () => {
   return (
-    <div>loading</div>
+    <div className='flex w-full px-8 gap-3 flex-col [&>div]:bg-wh-100 [&>div]:rounded-md'>
+      <div className='h-4 w-1/2' />
+      <div className='h-8 w-20' />
+      <div className='h-12 w-3/4' />
+      <div className='h-4 w-1/4' />
+      <div className='h-96 w-full' />
+      <div className='h-4 w-1/2' />
+      <div className='h-2 w-1/2' />
+      <div className='h-10 w-1/2' />
+      <div className='h-2 w-1/2' />
+    </div>
   )
 }
 
--- app/post/[id]/loading.tsx
+++ app/post/[id]/loading.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import { Skeleton } from '@/components/ui/skeleton'
-
 const loading = () => {
   return (
-    <div className='flex w-full px-8 gap-3 flex-col'>
-      <Skeleton className='h-4 w-1/2 bg-wh-100' />
-      <Skeleton className='h-8 w-20 bg-wh-100' />
-      <Skeleton className='h-12 w-3/4 bg-wh-100' />
-      <Skeleton className='h-4 w-1/4 bg-wh-100' />
-      <Skeleton className='h-96 w-full bg-wh-100' />
-      <Skeleton className='h-4 w-1/2 bg-wh-100' />
-      <Skeleton className='h-4 w-1/2 bg-wh-100' />
-      <Skeleton className='h-4 w-1/2 bg-wh-100' />
+    <div className='flex w-full px-8 gap-3 flex-col [&>div]:bg-wh-100 [&>div]:rounded-md'>
+      <div className='h-4 w-1/2' />
+      <div className='h-8 w-20' />
+      <div className='h-12 w-3/4' />
+      <div className='h-4 w-1/4' />
+      <div className='h-96 w-full' />
+      <div className='h-4 w-1/2' />
+      <div className='h-2 w-1/2' />
+      <div className='h-10 w-1/2' />
+      <div className='h-2 w-1/2' />
     </div>
   )
 }
