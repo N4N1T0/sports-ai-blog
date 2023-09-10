1c7aa3917e84e321037f3ffeacf3239916ad0068
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2023-09-10 23:17:04 +0200

    added skeletons in loading of about, post and trending

--- app/about/loading.tsx
+++ app/about/loading.tsx
@@ -1,15 +1,16 @@
 const loading = () => {
   return (
-    <div className='flex w-full px-8 gap-3 flex-col [&>div]:bg-wh-100 [&>div]:rounded-md'>
-      <div className='h-4 w-1/2' />
-      <div className='h-8 w-20' />
-      <div className='h-12 w-3/4' />
-      <div className='h-4 w-1/4' />
-      <div className='h-96 w-full' />
-      <div className='h-4 w-1/2' />
-      <div className='h-2 w-1/2' />
-      <div className='h-10 w-1/2' />
-      <div className='h-2 w-1/2' />
+    <div className='flex w-full px-8 gap-3'>
+      <div className='basis-3/4 gap-5 flex flex-col [&>div]:bg-wh-100 [&>div]:rounded-md'>
+        <div className='h-6 w-full' />
+        <div className='h-14 w-full' />
+        <div className='h-96 w-full' />
+        <div className='h-96 w-full' />
+      </div>
+      <div className='basis-1/4 [&>div]:bg-wh-100 [&>div]:rounded-md'>
+        <div className='w-full h-96'/>
+        <div className='w-full h-96'/>
+      </div>
     </div>
   )
 }
--- app/trending/loading.tsx
+++ app/trending/loading.tsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function loading () {
   return (
-    <div>loading</div>
+    <div className='flex w-full px-8 gap-3'>
+      <div className='basis-3/4 flex flex-col gap-5'>
+        <div className='h-10 w-full bg-wh-100 rounded-md' />
+        <div className='h-96 w-full flex gap-5 [&>div]:bg-wh-100 [&>div]:rounded-md'>
+          <div className='w-3/4 h-full' />
+          <div className='w-3/4 h-full' />
+        </div>
+        <div className='h-96 w-full flex gap-5 [&>div]:bg-wh-100 [&>div]:rounded-md'>
+          <div className='w-3/4 h-full' />
+          <div className='w-3/4 h-full' />
+        </div>
+      </div>
+      <div className='basis-1/4 flex flex-col gap-5 [&>div]:bg-wh-100 [&>div]:rounded-md'>
+        <div className='w-full h-96'/>
+        <div className='w-full h-96'/>
+      </div>
+    </div>
   )
 }
 
